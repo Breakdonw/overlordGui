05472d9eccbd4f9f07818f8a4ef37b61b65b5b90
Breakdonw <ably159@gmail.com>
2022-05-07 00:42:06 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import {NgbConfig} from '@ng-bootstrap/ng-bootstrap';
 
 /* The AppModule class is the root module of the application */
 @NgModule({
@@ -11,9 +14,20 @@ import { AppComponent } from './app.component';
   imports: [
 
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(ngbConfig: NgbConfig) {
+    ngbConfig.animation = false;
+  }
+ }
